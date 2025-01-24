33a145f98be272bad968ff829a1c8aadeb9c4255
kmccol1 <kmccol1@users.noreply.github.com>
2025-01-24 20:46:23 +0000

    deploy: ed14f42294c0723225e28e9da23dbc536a583891

--- jest.setup.js
+++
@@ -1,4 +0,0 @@
-// jest.setup.js
-const { TextEncoder, TextDecoder } = require('util');
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
--- main.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-from app.routes import bp
-
-app = create_app()  # Initialize the app using the create_app() function.
-app.register_blueprint(bp)
-
-if __name__ == '__main__':
-    app.run(debug=True)
