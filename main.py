c6494332c7ccfdd31eb8d8f3ffcf5e1e0c92f331
Kyle McColgan <kdmbus21@gmail.com>
2025-01-27 17:59:38 -0600

    main.py: Introduce a BASE_PATH environment variable, and align the HTML templates.

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ from app.routes import bp as main_routes_bp  # Import with an alias.
 
 app = create_app()  # Initialize the app using the create_app() function.
 
+# Set base path depending on environment
+app.config['BASE_PATH'] = '/myTimer' if os.environ.get('FLASK_ENV') == 'production' else '/'
+app.jinja_env.globals['base_path'] = app.config['BASE_PATH']
+
 # Add the Flask CLI command to render templates into static HTML...
 @app.cli.command("render-static")
 def render_static():
